80fea6c24d563000edfb867bc77c6e58154af1c5
Alessadro Esposito Brescia <alessandro.esposito.brescia@gmail.com>
2022-11-21 19:37:38 -0300

    my activities page

--- src/Pages/MyActivities/index.js
+++ src/Pages/MyActivities/index.js
@@ -1,6 +1,38 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 export function MyActivities (){
+
+    const [activities, setActivities] = useState([])
+
+    useEffect(() => {
+        async function fetchActivity(){
+            try {
+                const response = await axios.get("https://ironrest.cyclic.app/just-do-it")
+                
+                setActivities(response.data)
+
+            } catch (err){
+                console.log(err)
+            }
+        }
+
+        fetchActivity()
+    }, [])
+
     return (
         <>
+        <h1>My Activities</h1>
+
+        {activities.map((currentActivity)=> {
+            return <div key={currentActivity._id}>
+                <h4>{currentActivity.activity}</h4>
+                <p>{currentActivity.type}</p>
+                <p>{currentActivity.paticipants}</p>
+                <p>{currentActivity.accessibility}</p>
+                <p>{currentActivity.price}</p>
+            </div>
+        })}
         </>
     )
 }
