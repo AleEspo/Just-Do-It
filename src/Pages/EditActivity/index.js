25909358ff80810fd8ed7c89ef9ab11b9a1bfb0b
Lucas <lucas.abc@protonmail.com>
2022-11-22 11:48:05 -0300

    add update

--- src/Pages/EditActivity/index.js
+++ src/Pages/EditActivity/index.js
@@ -1,7 +1,24 @@
 import { useParams } from "react-router-dom";
+import axios from "axios"
+import { useState, useEffect } from "react"
 
-export function EditActivity(){
+export function EditActivity() {
     const params = useParams();
+
+    const [activity, setActivities] = useState({})
+
+    useEffect(() => {
+        async function fetchActivity() {
+            try {
+                const response = await axios.get(`https://ironrest.cyclic.app/just-do-it/${params.id}`
+                );
+                setActivities(response.data);
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    }, []);
+
     return (
         <h1>Aqui a gente vai modificar as notas</h1>
     )
