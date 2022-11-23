86fbc2441f9dfa38f93e52f875443cb4e8ef06d8
Alessadro Esposito Brescia <alessandro.esposito.brescia@gmail.com>
2022-11-23 13:12:17 -0300

    edit create form

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -40,6 +40,8 @@ export function Home() {
 
   const [activities, setActivities] = useState([]);
   const [filteredActivities, setFilteredActivities] = useState([])
+  const myActivities = []
+  {console.log(myActivities)}
 
   useEffect(() => {
     async function fetchActivity() {
@@ -76,25 +78,36 @@ export function Home() {
           <label htmlFor="input-accessibility">Accessibility</label>
           <input
             id="input-accessibility"
-            type="text"
+            type="number"
             name="accessibility"
             onChange={handleChange}
             value={form.accessibility}
           />
 
           <label htmlFor="input-type">Type</label>
-          <input
+          <select id="input-type">
+            <option>Education</option>
+            <option>Recreational</option>
+            <option>Social</option>
+            <option>DIY</option>
+            <option>Charity</option>
+            <option>Cooking</option>
+            <option>Relaxation</option>
+            <option>Music</option>
+            <option>Busy Work</option>
+          </select>
+          {/* <input
             id="input-type"
             type="text"
             name="type"
             onChange={handleChange}
             value={form.type}
-          />
+          /> */}
 
           <label htmlFor="input-participants">Participants</label>
           <input
             id="input-participants"
-            type="text"
+            type="number"
             name="participants"
             onChange={handleChange}
             value={form.participants}
@@ -104,7 +117,7 @@ export function Home() {
           <input
             id="input-price"
             type="text"
-            name="price"
+            name="number"
             onChange={handleChange}
             value={form.price}
           />
@@ -112,20 +125,26 @@ export function Home() {
           <label htmlFor="input-availability">Availability</label>
           <input
             id="input-availability"
-            type="text"
+            type="number"
             name="availability"
             onChange={handleChange}
             value={form.availability}
           />
 
           <label htmlFor="input-duration">Duration</label>
-          <input
+          <select>
+            <option>Minutes</option>
+            <option>Hours</option>
+            <option>Days</option>
+            <option>Weeks</option>
+          </select>
+          {/* <input
             id="input-duration"
             type="text"
             name="duration"
             onChange={handleChange}
             value={form.duration}
-          />
+          /> */}
 
           <label htmlFor="input-link">Link</label>
           <input
@@ -139,22 +158,22 @@ export function Home() {
           <label htmlFor="input-kids">Kid Friendly</label>
           <input
             id="input-kids"
-            type="boolean"
+            type="radio"
             name="kidFriendly"
             onChange={handleChange}
             value={form.kidFriendly}
           />
 
-          <button>Submit Activity</button>
+          <button>Create new Activity</button>
         </form>
         <Link to="/my-activities">My Activities</Link>
       </div>
       <div>
-        <h2>Form para filtrar as atividades</h2>
+        <h2>Search activity (form para filtrar as atividades)</h2>
         < SearchActivity filteredFunction={setFilteredActivities} allActivities={activities}/>
       </div>
       <div>
-        <h2>Lista de atividades do API</h2>
+        <h2>All activities (lista de atividades do API)</h2>
         {filteredActivities.map((currentActivity) => {
           return (
             <div key={currentActivity._id}>
@@ -166,6 +185,9 @@ export function Home() {
               <p>
                 Kids Friendly: {currentActivity.kidFriendly ? "Yes" : "No  "}
               </p>
+              <button onClick={()=>{
+                myActivities.push(currentActivity)
+                console.log(myActivities)}} >Add to my activities</button>
             </div>
           );
         })}
@@ -173,83 +195,3 @@ export function Home() {
     </>
   );
 }
-
-// import { useEffect, useState } from "react"
-// import axios from "axios"
-// import { Link } from "react-router-dom"
-
-// export function Home (){
-
-//     const [formSubmitted, setFormSubmitted] = useState(false)
-
-//     const [form, setForm] = useState({
-//         activity: "",
-//         accessibility: 0,
-//         type: "",
-//         participants: 1,
-//         price: 0,
-//         link: "",
-//     })
-
-//     function handleChange(e){
-//         setForm({...form, [e.target.name]: e.target.value})
-//     }
-
-//     async function handleSubmit(e){
-
-//         e.preventDefault()
-
-//         try {
-//             const response = await axios.post("https://ironrest.cyclic.app/just-do-it", {...form, })
-//             setFormSubmitted(true)
-//         } catch (err){
-//             console.log(err)
-//         }
-//     }
-
-//     useEffect(()=>{
-//         function returnForm(){
-//             setFormSubmitted(false)
-//             return (
-//                 <>
-//                     <h1>Just Do It</h1>
-
-//                     <form onSubmit={handleSubmit}>
-//                         <label htmlFor="input-activity">Activity</label>
-//                         <input id="input-activity" type="text" name="activity" onChange={handleChange} value={form.activity}/>
-
-//                         <label htmlFor="input-accessibility">Accessibility</label>
-//                         <input id="input-accessibility" type="text" name="accessibility" onChange={handleChange} value={form.accessibility}/>
-
-//                         <label htmlFor="input-type">Type</label>
-//                         <input id="input-type" type="text" name="type" onChange={handleChange} value={form.type}/>
-
-//                         <label htmlFor="input-participants">Participants</label>
-//                         <input id="input-participants" type="text" name="participants" onChange={handleChange} value={form.participants}/>
-
-//                         <label htmlFor="input-price">Price</label>
-//                         <input id="input-price" type="text" name="price" onChange={handleChange} value={form.price}/>
-
-//                         <label htmlFor="input-availability">Availability</label>
-//                         <input id="input-availability" type="text" name="availability" onChange={handleChange} value={form.availability}/>
-
-//                         <label htmlFor="input-duration">Duration</label>
-//                         <input id="input-duration" type="text" name="duration" onChange={handleChange} value={form.duration}/>
-
-//                         <label htmlFor="input-link">Link</label>
-//                         <input id="input-link" type="text" name="link" onChange={handleChange} value={form.link}/>
-
-//                         <label htmlFor="input-kids">Kid Friendly</label>
-//                         <input id="input-kids" type="boolean" name="kidFriendly" onChange={handleChange} value={form.kidFriendly}/>
-
-//                         <button>Submit Activity</button>
-//                     </form>
-//                     <Link to="/my-activities">My Activities</Link>
-//                 </>
-//             )
-//         }
-
-//         returnForm()
-
-//     }, [formSubmitted])
-// }
